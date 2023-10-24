ff5f0feaf30ff3f85bdfb3fd1aa708ae1570e08f
awak282007 <148284425+awak282007@users.noreply.github.com>
2023-10-24 22:24:07 +0000

    Didn't finish.

--- operators.js
+++ operators.js
@@ -3,9 +3,16 @@
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
+let num1 = 2
+let num2 = 10
 // Add the two numbers and store the result in a new variable called sum.
-// Print the result using console.log().
+let add = num1 + num2 = 12
+let divide = num2 / num1 = 5
+let multiply = num1 * num2 = 20
+let subtract = num2 - num1 = 8
 
+// Print the result using console.log().
+console.log(add)
 
 
 // 2. Subtraction
--- variables.js
+++ variables.js
@@ -1,13 +1,22 @@
 // 1. Create a variable called `favoriteColor` and assign it a string value of your favorite color.
 let favoriteColor = "YOUR_COLOR_HERE";
 
+
+
 // 2. Create a variable called `favoriteFood` and assign it a string value of your favorite food.
+
+let favoriteFood = "Favorite_Food_Here";
 
 
 // 3. Create a variable called `favoriteNumber` and assign it a number value of your favorite number.
 
+let favoriteNumber = "Favorite_Nnumber_Here";
+
 
 // 4. Create a variable called `isSunnyToday` and assign it a boolean value based on if it's sunny today.
+
+let isSunnyToday = "Is-Sunny-Today"
+
 
 
 // 5. Console log each variable on a new line.
